0074c683efc861e92d73dfdec5d70610a3ce7eec
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2021-06-09 22:33:49 +0700

    [kyomel] data type string done

---
+++ string/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Michael")
+	fmt.Println("Michael Stevan")
+	fmt.Println("Michael Stevan Lapandio")
+
+	fmt.Println(len("Michael"))
+	fmt.Println("Michael Stevan"[0])
+	fmt.Println("Michael Stevan Lapandio"[1])
+}
