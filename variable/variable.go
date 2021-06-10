5e0e9a2d802a2619bb5abb583637ead94649ac39
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2021-06-10 21:52:33 +0700

    [kyomel] write variable done

---
+++ variable/variable.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Michael Stevan"
+	fmt.Println(name)
+
+	name = "Michael Lapandio"
+	fmt.Println(name)
+
+	var friendName = "Budi"
+	fmt.Println(friendName)
+
+	var age = 30
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	country = "America"
+	fmt.Println(country)
+
+	var (
+		firstName = "Michael"
+		lastName  = "Stevan"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
