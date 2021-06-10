02fb9406936b8693a3974f214134ef9a6d8ad759
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2021-06-10 22:06:59 +0700

    [kyomel] constant variable done

---
+++ constant/constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string = "Michael"
+		lastName         = "Stevan"
+		value            = 1000
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
