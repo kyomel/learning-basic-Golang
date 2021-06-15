ea00c407d218fb60618b599e4cad76a11d4e9574
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-06-15 15:58:36 +0700

    [kyomel] map type is done

---
+++ map/map.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	person := map[string]string{
+		"name":    "Michael",
+		"address": "Poso",
+	}
+
+	person["title"] = "Programmer"
+
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["address"])
+	fmt.Println(person["title"])
+
+	var book map[string]string = make(map[string]string)
+	book["title"] = "Belajar Golang"
+	book["author"] = "Michael"
+	book["ups"] = "Salah"
+	fmt.Println(book)
+	fmt.Println(len(book))
+
+	delete(book, "ups")
+
+	fmt.Println(book)
+	fmt.Println(len(book))
+}
