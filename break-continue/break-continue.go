d1d409b116a0c419373c035019573f88801ff439
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-06-16 16:56:51 +0700

    [kyomel] break-continue is done

---
+++ break-continue/break-continue.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// Break statement
+	// for i := 0; i < 10; i++ {
+	// 	if i == 5 {
+	// 		break
+	// 	}
+	// 	fmt.Println("Perulangan ke ", i)
+	// }
+
+	// Continue statement
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 == 1 {
+	// 		continue
+	// 	}
+	// 	fmt.Println("Perulangan ke", i)
+	// }
+}
