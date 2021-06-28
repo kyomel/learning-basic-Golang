d85b79668aecd1009382920df0c31e60044b2bad
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2021-06-28 12:12:15 +0700

    [kyomel] access modifier is done

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,18 @@ package helper
 
 import "fmt"
 
+var version = 1
+var Application = "Belajar Golang"
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+// function tidak dapat diekspor karena dimulai dengan huruf kecil
+// func sayGoodbye(name string) {
+// 	fmt.Println("Goodbye", name)
+// }
+
+func SayGoodbye(name string) {
+	fmt.Println("Goodbye", name)
 }
--- import.go
+++ import.go
@@ -1,7 +1,13 @@
 package main
 
-import "Basic-Golang/helper"
+import (
+	"Basic-Golang/helper"
+	"fmt"
+)
 
 func main() {
 	helper.SayHello("Michael")
+	helper.SayGoodbye("Michael")
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // error
 }
