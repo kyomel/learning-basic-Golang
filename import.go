7fa539fd9dc1b70c78d197ec7b5ae82a112adb8a
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2021-06-28 13:37:09 +0700

    [kyomel] package initialization is done

--- import.go
+++ import.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"Basic-Golang/database"   // package inizialization
+	_ "Basic-Golang/database" // blank identifier
 	"Basic-Golang/helper"
 	"fmt"
 )
@@ -10,4 +12,8 @@ func main() {
 	helper.SayGoodbye("Michael")
 	fmt.Println(helper.Application)
 	// fmt.Println(helper.version) // error
+
+	// part package inizialization
+	result := database.GetDatabase()
+	fmt.Println(result)
 }
